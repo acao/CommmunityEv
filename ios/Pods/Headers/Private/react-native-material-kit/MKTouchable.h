2d5ccf4b14d95b0609f5477945984be1a7c0bbb0
Richard Schulte <rikki@mrkrs.co>
2016-01-08 09:02:28 -0500

    cleanup cocopods

--- ios/Pods/Headers/Private/react-native-material-kit/MKTouchable.h
+++
@@ -1,1 +0,0 @@
-../../../../../node_modules/react-native-material-kit/iOS/RCTMaterialKit/MKTouchable.h
--- ios/Pods/Target Support Files/react-native-material-kit/react-native-material-kit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_react_native_material_kit : NSObject
-@end
-@implementation PodsDummy_react_native_material_kit
-@end
